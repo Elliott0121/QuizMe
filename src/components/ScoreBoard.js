daaf55e5eb1c80b22770a02c21998cfa56c24deb
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2020-05-23 20:41:01 +0200

    Scoreboard changes

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 
 export class ScoreBoard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            round5: JSON.parse(localStorage.getItem(`High_Score_5`)),
+            round10: JSON.parse(localStorage.getItem(`High_Score_10`)),
+            roundSpeed: JSON.parse(localStorage.getItem(`High_Score_Speed`))
+        }
+    }
     render() {
-        let round5 = JSON.parse(localStorage.getItem(`High_Score_5`))
-        let round10 = JSON.parse(localStorage.getItem(`High_Score_10`))
+        let array = [this.state.round5, this.state.round10, this.state.roundSpeed]
         return (
             <div className="ui segment">
                 <div className="ui header center aligned">
-                    <h1 class="ui red header">Scoreboard</h1>
+                    <h1 className="ui header">Scoreboard</h1>
                 </div>
                 <div className="ui divider"></div>
-                {round10 == null || round5 == null ? <h2 class="ui header center aligned">No high-scores saved. Play a game to receive a high-score!</h2> :
+                {array == '' ? <h2 class="ui header center aligned">No high-scores saved. Play a game to receive a high-score!</h2> :
                     <div id="Scoreboard">
                         <div style={{ textAlign: 'left' }}>
                             <div id="scoreboard-title">
-                                <table class="ui celled inverted selectable table">
+                                <table className="ui celled inverted selectable table">
                                     <thead className="">
                                         <tr className="">
                                             <th className="">Rounds<i id="scoreboard-icon" aria-hidden="true" className="hourglass half icon" ></i></th>
@@ -23,16 +30,14 @@ export class ScoreBoard extends Component {
                                         </tr>
                                     </thead>
                                     <tbody className="">
-                                        <tr className="">
-                                            <td className="">5</td>
-                                            <td className="">{round5.High_Score}</td>
-                                            <td className="">{round5.Date}</td>
-                                        </tr>
-                                        <tr className="">
-                                            <td className="">10</td>
-                                            <td className="">{round10.High_Score}</td>
-                                            <td className="">{round10.Date}</td>
-                                        </tr>
+                                        {Object.values(array).map((val, i) => (
+                                            val == null ? '' :
+                                                <tr key={i}>
+                                                    <td>{val.Rounds}</td>
+                                                    <td>{val.High_Score}</td>
+                                                    <td>{val.Date}</td>
+                                                </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
